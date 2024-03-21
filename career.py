b16e2bba49beca307f9ff862718865e6e7cd25eb
4FTSQRL <156530576+4FTSQRL@users.noreply.github.com>
2024-03-20 22:33:46 -0400

    Create career.py
    set up the script

---
+++ career.py
@@ -0,0 +1,31 @@
+"""
+Author: Elicia Ramitt
+
+Script: career.py
+
+Description: Gets the careers of people and then shows if they are retired, currently working in field, or in school
+
+Usage: python career.py
+"""
+
+# Import statments
+    # os
+import os
+    # Get dbPath, createPeople, scriptDirectory from createDB.py
+from createDB import scriptDirectory, dbPath, createPeople
+    # sqlite3
+import sqlite3
+    # pandas
+import pandas
+    # Choice
+from random import choice
+
+
+# Main Function
+def main():
+    # Return Statement
+    return
+
+# Python Incantation
+if __name__ == "__main__":
+    main()
