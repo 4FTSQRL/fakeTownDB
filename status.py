9818fea4e409373a29718a806902012c8c1b4f0f
4FTSQRL <156530576+4FTSQRL@users.noreply.github.com>
2024-03-22 15:09:23 -0400

    Update status.py
    Completed getAlive() function

--- status.py
+++ status.py
@@ -20,9 +20,55 @@ import pandas
 
 # Create main function
 def main():
+    # Call user choice function
+    userChoice()
     # Return statement
     return
 
+# User Choice
+def userChoice():
+    # Prompt the user to choose an option
+    choice = input("Choose an option from the list belo:\n"
+                   "1. Export a list of people who are alive to CSV\n"
+                   "2. Export a list of people who are deceased to CSV\n"
+                   "3. Exit\n"
+                   "Enter your choice: ")
+
+# Function to get citzens who are alive
+def getAlive():
+    """
+    Description: Searches people for the citzens who are alive
+    
+    Returns: alive (name, age, status)
+    """
+    
+    # Get the people table
+    people = createPeople()
+    
+    # Connect to dbPath
+    con = sqlite3.connect(dbPath)
+    
+    # Cursor
+    cur = con.cursor()
+    
+    # Execute to get all the data from people
+    cur.execute("SELECT * from people")
+    
+    # Fetch it
+    people = cur.fetchall()
+    
+    # List for alive people
+    alive = []
+    
+    # Search for people who are alive through a for loop
+    for person in people:
+        # Check status
+        if person[4] == "Alive":
+            # Append to the alive list
+            alive.append((person[1], person[2], person[4]))
+    
+    # Return Statement
+    alive
 # Python Incantation
 if __name__ == "__main__":
     main()
