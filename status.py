1923f023332d16347e0ca3f7bb9b9c9d397eeb76
4FTSQRL <156530576+4FTSQRL@users.noreply.github.com>
2024-03-22 14:56:57 -0400

    Create status.py
    script set-up

---
+++ status.py
@@ -0,0 +1,28 @@
+"""
+Author: Elicia Ramitt
+
+Script: status.py
+
+Description: Get all people who are alive and deceased
+
+Usage: python status.py    
+"""
+
+# Import Statements
+    # os
+import os
+    # Get dbPath, createPeople, scriptDirectory from createDB.py
+from createDB import scriptDirectory, dbPath, createPeople
+    # sqlite3
+import sqlite3
+    # pandas
+import pandas
+
+# Create main function
+def main():
+    # Return statement
+    return
+
+# Python Incantation
+if __name__ == "__main__":
+    main()
